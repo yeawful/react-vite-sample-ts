f27fa2c77078b6fac5bb66f8a94a04eeccfa0b65
Ilya Raketa <paketa1337@gmail.com>
2025-07-02 16:45:49 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App.js';
+import './styles/index.scss'
+import '@a1rth/css-normalize'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
     plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/variables" as *;`,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
